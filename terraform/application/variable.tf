d3f13c4f9ad7b7cc0b44609f1b4a477852e08ae1
comkar893 <comkar893@gmail.com>
2024-03-22 20:57:58 -0400

    Terraform Script to Run Flask App on EC2

---
+++ terraform/application/variable.tf
@@ -0,0 +1,39 @@
+variable "backend_instance_type" {
+    type        = string
+    description = "Instance type for running Backend"
+    default     = "t2.micro"
+}
+
+locals {
+    aws_key_value_pair = "<Your Key Value Pair>" # Change this to your Key-Value Pair
+  }
+
+variable "github_username" {
+    type        = string
+    description = "GitHub Username"
+    default     = "<Repository Admin's Username>" # Change this to Repositoy Admin's Username
+}
+
+variable "github_pat" {
+    type        = string
+    description = "GitHub Personal Access Token"
+    default     = "<Repository Admin's PAT>" # Change this to Repositoy Admin's Personal Access Token
+}
+
+variable "aws_region" {
+    type        = string
+    description = "AWS Region"
+    default     = "<Desired Region>" # Change this to your desired region e.g 'us-east-1'
+}
+
+variable "aws_access_key" {
+    type        = string
+    description = "AWS Access Keys"
+    default     = "<Your Root Access Key>" # Change this to your Root Access Key
+}
+
+variable "aws_secret_access_key" {
+    type        = string
+    description = "AWS Secret Access Keys"
+    default     = "<Your Root Secret Access Key>" # Change this to your Root Secret Access Key
+}
